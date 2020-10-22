a2968a43a0c3b3574e66fd867c72afed68e43922
Tawhid Hannan <t.hannan93@gmail.com>
2020-10-22 16:35:39 +0100

    Construct IP struct from IP address strings

---
+++ pkg/v1/ipv4/ip.go
@@ -0,0 +1,42 @@
+package ipv4
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+type IP struct {
+}
+
+var (
+	ipFormatError       = errors.New("Invalid IPv4 address provided to constructor")
+	ipSegmentUint8Error = errors.New("IPv4 segments must be unsigned 8-bit integer values (i.e between 0 and 255 inclusive)")
+)
+
+func NewIPFromString(addr string) (*IP, error) {
+	segments := strings.Split(addr, ".")
+	buffer := [4]byte{}
+
+	if len(segments) != 4 {
+		return nil, ipFormatError
+	}
+
+	for i, segment := range segments {
+		val, err := strconv.Atoi(segment)
+		if err != nil {
+			return nil, err
+		}
+
+		// To prevent a panic, we'll verify that the
+		// value is indeed a uint8 before trying to
+		// cast it to a uint8
+		if val < 0 || val > 255 {
+			return nil, ipSegmentUint8Error
+		}
+
+		copy(buffer[:i], []uint8{uint8(val)})
+	}
+
+	return &IP{}, nil
+}
---
+++ pkg/v1/ipv4/ip_test.go
@@ -0,0 +1,34 @@
+package ipv4_test
+
+import (
+	"errors"
+	"go-cidr-toolkit/pkg/v1/ipv4"
+	"reflect"
+	"testing"
+)
+
+func TestNewIPFromString(t *testing.T) {
+	input := "192.168.0.1"
+	_, err := ipv4.NewIPFromString(input)
+	if err != nil {
+		t.Fatalf("Expected IP struct to be constructed without error for valid IPv4 address: %v, encountered error: %v", input, err)
+	}
+}
+
+func TestNewIPFromString_InvalidIPV4AddressFormat(t *testing.T) {
+	input := "192.168,0.1"
+	_, err := ipv4.NewIPFromString(input)
+	expected := errors.New("Invalid IPv4 address provided to constructor")
+	if !reflect.DeepEqual(expected, err) {
+		t.Fatalf("Expected error to be %v, received %v", expected, err)
+	}
+}
+
+func TestNewIPFromString_OutOfBoundsValue(t *testing.T) {
+	input := "257.168.0.1"
+	_, err := ipv4.NewIPFromString(input)
+	expected := errors.New("IPv4 segments must be unsigned 8-bit integer values (i.e between 0 and 255 inclusive)")
+	if !reflect.DeepEqual(expected, err) {
+		t.Fatalf("Expected error to be %v, received %v", expected, err)
+	}
+}
